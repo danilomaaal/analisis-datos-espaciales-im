d198a813904ae09256ed3f78c8cb138e1ef1b7f2
dm <daniel.malugo@gmail.com>
2026-06-14 22:40:07 -0600

    set up repo

---
+++ devenv.nix
@@ -0,0 +1,41 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  env.GREET = "devenv";
+
+  languages.r = {
+    enable = true;
+    lsp = {
+      enable = true;
+    };
+  };
+  
+
+  packages = [ 
+    pkgs.rPackages.tidyverse
+    pkgs.rPackages.leaflet
+    pkgs.rPackages.leaflet_extras # broken, to allow it run: export NIXPKGS_ALLOW_BROKEN=1; devenv shell --impure
+    pkgs.rPackages.rsconnect
+    pkgs.rPackages.sf
+    pkgs.rPackages.usethis
+    pkgs.rPackages.ellmer
+    pkgs.rPackages.mapedit
+    pkgs.rPackages.osrm
+    pkgs.rPackages.bivariateLeaflet
+    pkgs.rPackages.sfdep
+    pkgs.rPackages.htmlwidgets
+    pkgs.rPackages.janitor
+    pkgs.pandoc
+  ];
+
+
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello         # Run scripts directly
+    R --version
+  '';
+  
+}
